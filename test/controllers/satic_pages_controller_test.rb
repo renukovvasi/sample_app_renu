1493cddb5094ab7f21253937b4decf08e8ae181a
renukovvasi <renu.kovvasi@gmail.com>
2015-02-20 05:51:11 +0000

    sample app done in cloud 9

---
+++ test/controllers/satic_pages_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class SaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title","Ruby on Rails  Sample App"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title","Help | Ruby on Rails Sample App"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title","About | Ruby on Rails  Sample App"
+  end  
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails  Sample App"
+  end
+end
