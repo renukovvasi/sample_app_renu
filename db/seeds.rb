b651a37455eca823218a056e47c138526dcaf3aa
renukovvasi <renu.kovvasi@gmail.com>
2015-02-26 14:54:47 +0530

    add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,17 @@ User.create!(name:  "Renuka",
              email: "renu@gmail.com",
              password:              "12345",
              password_confirmation: "12345",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@gmail.com"
   password = "password"
-  User.create!(name:  name,
+  User.create!( name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now )
 end
